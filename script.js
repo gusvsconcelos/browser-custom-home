5e4443391b2ea6892f848f85027d43dcb0511c09
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-04-11 14:17:54 -0300

    HTML id elements bug fix

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function time() {
   const newDate = new Date();
-  const formattedDate = newDate.toLocaleString('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
+  const formattedDate = newDate.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
     hour12: true,
   });
-  const hour = document.getElementById('hour');
+  const hour = document.getElementById("hour");
 
   hour.innerHTML = formattedDate.replace(/:/g, '<span class="blink">:</span>');
 }
 
 function date() {
   const newDate = new Date();
-  const dayOfMonth = newDate.toLocaleDateString('pt-BR');
-  const date = document.getElementById('date');
+  const dayOfMonth = newDate.toLocaleDateString("pt-BR");
+  const date = document.getElementById("date");
 
   date.innerHTML = dayOfMonth;
 }
 
 async function getQuotes() {
   try {
-    const answer = await fetch('https://api.quotable.io/random');
+    const answer = await fetch("https://api.quotable.io/random");
     const data = await answer.json();
     return `${data.content}`;
   } catch (error) {
-    console.error('Error retrieving quote:', error);
+    console.error("Error retrieving quote:", error);
   }
 }
 
 async function quoteOfTheDay() {
-  const quoteElement = document.getElementById('quote');
+  const quoteElement = document.getElementById("quote");
   const quote = await getQuotes();
 
   quoteElement.textContent = `'${quote}'`;
@@ -43,17 +43,17 @@ async function quoteOfTheDay() {
 function darkTheme() {
   return (
     window.matchMedia &&
-    window.matchMedia('(prefers-color-scheme: dark').matches
+    window.matchMedia("(prefers-color-scheme: dark").matches
   );
 }
 
 function updateFavicon() {
-  const favicon = document.getElementById('favicon');
+  const favicon = document.getElementById("favicon");
 
   if (darkTheme()) {
-    favicon.href = 'icons/favicon-white.ico';
+    favicon.href = "icons/favicon-white.ico";
   } else {
-    favicon.href = 'icons/favicon-black.ico';
+    favicon.href = "icons/favicon-black.ico";
   }
 }
 
@@ -64,6 +64,6 @@ function functionLoader() {
   updateFavicon();
 }
 
-window.addEventListener('load', functionLoader, true);
+window.addEventListener("load", functionLoader, true);
 
 setInterval(time, 1000);
